be0e5e3bb2e5e7c02a5da96291c1784e2103534d
Oleksii Koshelenko <oleksii_koshelenko@epam.com>
2024-07-24 07:59:25 +0300

    delete commented code

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -6,7 +6,3 @@ output "storage_account_primary_access_key" {
   value     = azurerm_storage_account.storage.primary_access_key
   sensitive = true
 }
-
-# output "storage_container_name" {
-#   value = azurerm_storage_container.container.name
-# }
--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,6 @@ output "storage_account_name" {
   value = module.storage.storage_account_name
 }
 
-# output "storage_container_name" {
-#   value = module.storage.storage_container_name
-# }
-
 output "mysql_server_fqdn" {
   value = module.database.mysql_server_fqdn
 }
